d753db7085d9f01638cc0299952d76b0e7047147
viensolas <102194349+viensolas@users.noreply.github.com>
2022-12-13 14:51:25 -0600

    Javascript navBar scroll

--- main.js
+++ main.js
@@ -4,12 +4,21 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(navbarHeight);
-
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+//Scrolling to clicked html element
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null){return};
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth", block:"start"});
+})
+
