71e8c2682e547cd448f4f4c70ab96ea5e3322bff
viensolas <102194349+viensolas@users.noreply.github.com>
2022-12-14 02:38:40 -0600

    scroll to home contact

--- main.js
+++ main.js
@@ -13,12 +13,33 @@ document.addEventListener('scroll', () => {
 
 //Scrolling to clicked html element
 
+// const navbarMenu = document.querySelector('.navbar__menu');
+// navbarMenu.addEventListener('click', (event)=>{
+//   const target = event.target;
+//   const link = target.dataset.link;
+//   if (link == null){return};
+//   const scrollTo = document.querySelector(link);
+//   scrollTo.scrollIntoView({behavior: "smooth", block:"start"});
+// });
+
+// const $ =(tag)=>document.querySelector(`${tag}`);
+// $('.home__contact').addEventListener('click',(e)=>{
+//   $('#contact').scrollIntoView({behavior:'smooth'});
+// })
+
+function scrollIntoView(selector){
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior:'smooth'})
+}
+
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event)=>{
   const target = event.target;
   const link = target.dataset.link;
   if (link == null){return};
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: "smooth", block:"start"});
-})
+  scrollIntoView(link);
+});
 
+
+const homeContactBttn = document.querySelector('.home__contact')
+homeContactBttn.addEventListener('click', (e)=>scrollIntoView("#contact"))
